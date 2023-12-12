74a018d10117f08d333942199dae09c0fb8f6d5e
Alexis <alexishadjian@gmail.com>
2023-12-12 15:13:47 +0100

    first commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const mongoose = require("mongoose");
+mongoose.connect('mongodb://mongo/f1-timer');
+
+app.use(express.urlencoded());
+app.use(express.json());
+
+const userRoute = require('./routes/userRoute');
+
+app.use('/users', userRoute);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
