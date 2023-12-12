f236823b250271b9435b7fc193aecd930c4cfe21
Alexis <alexishadjian@gmail.com>
2023-12-12 15:44:01 +0100

    User register & login

---
+++ src/controllers/userController.js
@@ -0,0 +1,56 @@
+const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+
+exports.userRegister = async (req,res) => {
+
+    try {
+        let newUser = new User(req.body);
+        let user = await newUser.save();
+        res.status(201).json({message: `Utilisateur crée: ${user.email}`})
+    } catch (error) {
+        console.log(error);
+        res.status(401).json({message: "Requête invalide"});
+    }
+
+}
+
+exports.userLogin = async (req,res) => {
+    try {
+        const user = await User.findOne({email: req.body.email});
+
+        if (!user) {
+            res.status(500).json({message: "Utilisateur non trouvé"});
+            return;
+        }
+
+        if (user.email === req.body.email && user.password === req.body.password) {
+            const userData = {
+                id: user._id,
+                email: user.email,
+                role: 'admin'
+            }
+            const token = await jwt.sign(userData, process.env.JWT_KEY, {expiresIn: "10h"})
+            res.status(200).json({token});
+
+        } else {
+            res.status(401).json({message: "Email ou mot de passe incorrect"});
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: "Une erreur s'est produite lors du traitement"})
+    }
+}
+
+exports.userUpdate = async (req,res) => {
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.user_id, req.body, {new: true});
+        res.status(200);
+        res.json(user);
+    } catch (error) {
+        res.status(500);
+        console.log(error);
+        res.json({message: 'erreur serveur'});
+    }
+
+}
---
+++ src/routes/userRoute.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const userController = require('../controllers/userController');
+
+
+router
+    .route('/register')
+    .post(userController.userRegister)
+
+
+router
+    .route('/login')
+    .post(userController.userLogin)
+
+module.exports = router;
